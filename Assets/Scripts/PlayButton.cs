80816d6231fb3312b5fc38e44f46e97f4f47fac6
kabirvirji <>
2020-02-22 15:56:13 -0500

    basic start screen ui button

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        SceneManager. LoadScene("Level1");
+
+    }
+}
