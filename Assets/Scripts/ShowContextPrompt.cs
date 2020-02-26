fe31bdda2d80be2087c257e4b8b2efa215967360
kabirvirji <>
2020-02-26 18:19:14 -0500

    start screen and context promots

---
+++ Assets/Scripts/ShowContextPrompt.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowContextPrompt : MonoBehaviour
+{
+    public GameObject contextPrompt;
+    void Start()
+    {
+        contextPrompt.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player") 
+        {
+            contextPrompt.SetActive(true);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.tag == "Player") 
+        {
+            contextPrompt.SetActive(false);
+        }
+    }
+
+}
