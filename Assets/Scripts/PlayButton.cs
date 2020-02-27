ec39022d106520eec2b77edf58eae0907ee0b8bb
kabirvirji <>
2020-02-27 14:59:45 -0500

    added start screen

--- Assets/Scripts/PlayButton.cs
+++ Assets/Scripts/PlayButton.cs
@@ -11,7 +11,7 @@ public class PlayButton : MonoBehaviour
         if (Input.anyKey)
         {
             // whatever our first scene is called
-            SceneManager. LoadScene("Level1Tester"); 
+            SceneManager. LoadScene("Level_1_Feb25"); 
         }
     }
 }
